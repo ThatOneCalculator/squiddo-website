f087e1a184a4d13f07c0c02b4ed102c5ccac19be
ThatOneCalculator <kainoa@t1c.dev>
2022-02-24 18:19:57 -0800

    Fix typewriter going too early

--- loading.js
+++ loading.js
@@ -1,13 +1,38 @@
-document.onreadystatechange = function() {
+document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
         document.querySelector(
-          "body").style.visibility = "hidden";
+            "body").style.visibility = "hidden";
         document.querySelector(
-          "#loader").style.visibility = "visible";
+            "#loader").style.visibility = "visible";
     } else {
         document.querySelector(
-          "#loader").style.display = "none";
+            "#loader").style.display = "none";
         document.querySelector(
-          "body").style.visibility = "visible";
+            "body").style.visibility = "visible";
+        const typeText = document.querySelector(".typeText")
+        let textToBeTyped = "* HEY [[EVERY!!]]\n* IT'S ME, [TheRealSquiddo] WITH SOME SPECIL DEALS!!"
+        if (screen.width >= 640) {
+            textToBeTyped += "\n* DON;T FORGET TO [L<IKE AND SUSCRIBE!!]"
+        }
+        let index = 0, isAdding = true
+
+        function playAnim() {
+            setTimeout(function () {
+                typeText.innerText = textToBeTyped.slice(0, index)
+                if (isAdding) {
+                    if (index > textToBeTyped.length) {
+                        isAdding = false
+                        setTimeout(function () {
+                            playAnim()
+                        }, 2000)
+                        return
+                    } else {
+                        index++
+                    }
+                }
+                playAnim()
+            }, 50)
+        }
+        playAnim()
     }
 };
