02bf0fee0c78867fe3435cf3ef34f1b403417ad9
ThatOneCalculator <kainoa@t1c.dev>
2022-02-24 20:48:38 -0800

    Formatting

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
-        document.querySelector(
-            "body").style.visibility = "hidden";
-        document.querySelector(
-            "#loader").style.visibility = "visible";
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
     } else {
-        document.querySelector(
-            "#loader").style.display = "none";
-        document.querySelector(
-            "body").style.visibility = "visible";
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
         const typeText = document.querySelector(".typeText")
         let textToBeTyped = "* HEY [[EVERY!!]]\n* IT'S ME, [TheRealSquiddo] WITH SOME SPECIL DEALS!!"
         if (screen.width >= 640) {
@@ -17,21 +13,21 @@ document.onreadystatechange = function () {
         let index = 0, isAdding = true
 
         function playAnim() {
-            setTimeout(function () {
+            setTimeout((function () {
                 typeText.innerText = textToBeTyped.slice(0, index)
                 if (isAdding) {
                     if (index > textToBeTyped.length) {
                         isAdding = false
-                        setTimeout(function () {
+                        setTimeout((function () {
                             playAnim()
-                        }, 2000)
+                        }), 2000)
                         return
                     } else {
                         index++
                     }
                 }
                 playAnim()
-            }, 50)
+            }), 50)
         }
         playAnim()
     }
