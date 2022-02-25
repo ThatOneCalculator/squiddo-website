692644e5c72f5d7795b01e1daf88f54f7f0bdac4
ThatOneCalculator <kainoa@t1c.dev>
2022-02-24 16:21:21 -0800

    Improve typewriter text

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,5 @@
 const typeText = document.querySelector(".typeText")
-const textToBeTyped = "* HEY [[EVERY!!]] IT'S ME, YOUR PAL [TheRealSquiddo] WITH SOME SPECIL DEALS!! DON;T FORGET TO [L<IKE AND SUSCRIBE!!]"
+const textToBeTyped = "* HEY [[EVERY!!]]\n* IT'S ME, [TheRealSquiddo] WITH SOME SPECIL DEALS!!\n* DON;T FORGET TO [L<IKE AND SUSCRIBE!!]"
 let index = 0, isAdding = true
 
 function playAnim() {
